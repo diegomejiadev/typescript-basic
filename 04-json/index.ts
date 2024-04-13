f54ccb27446ff35d1e70fe0d8728f34d9451170d
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-04-13 00:10:55 -0500

    feat: Typescript with JSON

---
+++ 04-json/index.ts
@@ -0,0 +1,31 @@
+const variable_json = {
+  a: 1,
+  b: 2,
+  c: {
+    a: 2,
+    b: 2,
+  },
+};
+
+// console.log('JSON', variable_json);
+// console.log('a variable', variable_json.a);
+// console.log('b nested', variable_json.c.b);
+
+//* =======================================
+
+const person_json = {
+  name: 'Diego',
+  lastname: 'Mejia',
+  age: 25,
+  skills: ['NodeJs', 'React', 'Typescript'],
+  inventory: {
+    tech: {
+      type: 'Computer',
+      brand: 'Intel',
+    },
+  },
+};
+
+// console.log(person_json.name);
+// console.log(person_json.inventory);
+// console.log(person_json.skills);
