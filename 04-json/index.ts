7f00ed3c966d5470c1afe68cd1fa997c1a1a76af
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-04-13 00:31:44 -0500

    chore: Added interface for Dependency inversion

--- 04-json/index.ts
+++ 04-json/index.ts
@@ -29,3 +29,63 @@ const person_json = {
 // console.log(person_json.name);
 // console.log(person_json.inventory);
 // console.log(person_json.skills);
+
+interface IObject {
+  name: string;
+  color: 'blue' | 'red';
+  status: 'good' | 'bad';
+  measurements: {
+    height: number;
+    width: number;
+    unit: 'cm' | 'm';
+  };
+}
+
+const object: IObject = {
+  color: 'red',
+  measurements: {
+    height: 20,
+    width: 30,
+    unit: 'cm',
+  },
+  name: 'Pelota',
+  status: 'good',
+};
+
+interface SQLConnection {
+  dialect: string;
+  connect: () => Promise<void>;
+  port?: number;
+  storage?: string;
+  host?: string;
+  ssl: boolean;
+  debug: boolean;
+}
+
+let libreria_sql: any;
+let libreria_sqlite: any;
+
+const mySqlConnection: SQLConnection = {
+  connect: async () => {
+    await libreria_sql.connect('127.0.0.1:5432');
+  },
+  debug: false,
+  dialect: 'mysql',
+  ssl: false,
+  host: '127.0.0.1',
+  port: 5432,
+};
+
+const sqlLiteConnection: SQLConnection = {
+  connect: async () => {
+    await libreria_sqlite.start();
+  },
+  debug: true,
+  dialect: 'sqlite',
+  ssl: false,
+  storage: 'file:/.db',
+};
+
+function connect(adapter: SQLConnection) {
+  adapter.connect();
+}
